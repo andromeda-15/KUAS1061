2c099aa2b6ba22c3a56bf2b943eb393e85ba3c9b
James <james_ch_lee@mail.gss.com.tw>
2018-01-05 21:24:27 +0800

    check in first version

---
+++ FirstMVCProject/src/tw/edu/nuk/java2/controller/FirstController.java
@@ -0,0 +1,32 @@
+package tw.edu.nuk.java2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FirstController {
+	
+	@RequestMapping("/welcome/{member}")
+	public String welcomeMember(Model model, 
+			@PathVariable("member") String MemberName) {
+		
+		model.addAttribute("member", MemberName);
+	
+		
+		return "WelcomeView";
+	}
+	
+	@RequestMapping("/hello")
+	public String helloUser(Model model, 
+			@RequestParam(value="user", defaultValue="User") String UserName) {
+		
+		model.addAttribute("user", UserName);
+		
+		return "HelloView";
+		
+	}
+
+}
